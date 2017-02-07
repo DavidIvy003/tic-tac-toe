5f25e6ea29bb6712e11f764ab49cdfe27ff7ed55
David Ivy <Davidivy003@gmail.com>
2017-02-07 12:56:21 -0500

    Add test for previously existing computer reward state

--- test/computer.js
+++ test/computer.js
@@ -3,8 +3,10 @@ import { X, O, BLANK } from '../public/assets/javascript/config'
 import computer from '../public/assets/javascript/computer'
 
 describe('Computer', function() {
-  const stack = []
-  const Computer = new computer({ type: 'computer', symbol: X })
+  let Computer
+  beforeEach(() => {
+    Computer = new computer({ type: 'computer', symbol: X })
+  })
 
   describe('reward', function() {
     it('should update reward stack', function() {
@@ -13,6 +15,15 @@ describe('Computer', function() {
                      BLANK, BLANK, X]
       Computer.reward(state, 8, 10)
       assert.equal(Computer.getStateReward(state, 8), 10);
+    });
+
+    it('can update existing state', function() {
+      const state = [X, O, X,
+                     BLANK, O, BLANK,
+                     BLANK, O, X]
+      Computer.reward(state, 8, 10)
+      Computer.reward(state, 8, -20)
+      assert.equal(Computer.getStateReward(state, 8), -10);
     });
   });
 
