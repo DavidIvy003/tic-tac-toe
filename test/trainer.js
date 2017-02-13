22de5b7a0404d62262d3534990aa324a7b993965
David Ivy <Davidivy003@gmail.com>
2017-02-12 23:53:41 -0500

    Convert trainer test to es6 syntax

--- test/trainer.js
+++ test/trainer.js
@@ -3,7 +3,7 @@ import trainer from '../public/assets/javascript/trainer'
 import history from './fixtures/history/win'
 import { X, O, BLANK } from '../public/assets/javascript/config'
 
-describe('Trainer', function() {
+describe('Trainer', () => {
   let stack = []
   const Trainer = new trainer()
   const Computer = { reward: (state, choice, points) => {
@@ -13,36 +13,36 @@ describe('Trainer', function() {
     stack = []
   })
 
-  describe('win', function() {
+  describe('win', () => {
     beforeEach(() => {
       Trainer.win(Computer, history, X)
     })
 
-    it('should update computer state', function() {
-      assert.equal(stack.length, 16);
-    });
+    it('should update computer state', () => {
+      assert.equal(stack.length, 16)
+    })
 
-    it('should rotate the board to speed up learning', function() {
+    it('should rotate the board to speed up learning', () => {
       assert.deepEqual(stack[13].state, [ BLANK, BLANK, X,
                                           BLANK, O, O,
-                                          X, BLANK, X ]);
-      assert.equal(stack[13].choice, 7  );
+                                          X, BLANK, X ])
+      assert.equal(stack[13].choice, 7  )
       assert.deepEqual(stack[14].state, [ X, BLANK, BLANK,
                                           BLANK, O, BLANK,
-                                          X, O, X ]);
-      assert.equal(stack[14].choice, 3);
+                                          X, O, X ])
+      assert.equal(stack[14].choice, 3)
       assert.deepEqual(stack[15].state, [ X, BLANK, X,
                                           O, O, BLANK,
-                                          X, BLANK, BLANK ]);
-      assert.equal(stack[15].choice, 1);
+                                          X, BLANK, BLANK ])
+      assert.equal(stack[15].choice, 1)
     })
-  });
+  })
 
-  describe('lose', function() {
-    it('should update computer state', function() {
+  describe('lose', () => {
+    it('should update computer state', () => {
       Trainer.lose(Computer, history, O)
-      assert.equal(stack.length, 12);
-    });
-  });
+      assert.equal(stack.length, 12)
+    })
+  })
 
-});
+})
